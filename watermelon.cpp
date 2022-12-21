d377498b05fdaf5596c4c318c079a7ab3eb9417b
David Tan <davidtanline@gmail.com>
2022-12-20 17:28:35 -0800

    sheesh

---
+++ watermelon.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int val;
+    cin >> val;
+    if (val % 2 == 0 && val > 2)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+    return 0;
+}
